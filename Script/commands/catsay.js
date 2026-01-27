90da94f341d6ba84e9ac704ee40627390f9cc5cd
mdmamunislam1982 <mamunofficial3210@gmail.com>
2026-01-27 22:48:56 +0600

    Update catsay.js

--- Script/commands/catsay.js
+++ Script/commands/catsay.js
@@ -1,103 +1,25 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const path = require("path");
-
-module.exports = {
-  config: {
-    name: "emoji_voice",
-    version: "2.0.2",
-    author: "MOHAMMAD AKASH",
-    countDown: 5,
-    role: 0,
-    shortDescription: "ইমোজি দিলে কিউট মেয়ের ভয়েস পাঠাবে 😍",
-    longDescription: "এক ইমোজিতে একাধিক ভয়েস, র‍্যান্ডমলি পাঠাবে 😘",
-    category: "noPrefix"
-  },
-
-  onStart: async function () {},
-
-  onChat: async function ({ event, message }) {
-    const { body } = event;
-    if (!body || body.length > 2) return;
-
-    // --- EMOJI WITH MULTIPLE RANDOM AUDIO LINKS ---
-    const emojiAudioMap = {
-      "🥱": ["https://files.catbox.moe/9pou40.mp3","https://files.catbox.moe/60cwcg.mp3"],
-      "😁": ["https://files.catbox.moe/60cwcg.mp3"],
-      "😌": ["https://files.catbox.moe/epqwbx.mp3"],
-      "🥺": ["https://files.catbox.moe/wc17iq.mp3","https://files.catbox.moe/dv9why.mp3"],
-      "🤭": ["https://files.catbox.moe/cu0mpy.mp3"],
-      "😅": ["https://files.catbox.moe/jl3pzb.mp3"],
-      "😏": ["https://files.catbox.moe/z9e52r.mp3"],
-      "😞": ["https://files.catbox.moe/tdimtx.mp3"],
-      "🤫": ["https://files.catbox.moe/0uii99.mp3"],
-      "🍼": ["https://files.catbox.moe/p6ht91.mp3"],
-      "🤔": ["https://files.catbox.moe/hy6m6w.mp3"],
-      "🥰": ["https://files.catbox.moe/dv9why.mp3"],
-      "🤦": ["https://files.catbox.moe/ivlvoq.mp3"],
-      "😘": ["https://files.catbox.moe/sbws0w.mp3","https://files.catbox.moe/37dqpx.mp3"],
-      "😑": ["https://files.catbox.moe/p78xfw.mp3"],
-      "😢": ["https://files.catbox.moe/shxwj1.mp3"],
-      "🙊": ["https://files.catbox.moe/3bejxv.mp3"],
-      "🤨": ["https://files.catbox.moe/4aci0r.mp3"],
-      "😡": ["https://files.catbox.moe/shxwj1.mp3","https://files.catbox.moe/h9ekli.mp3"],
-      "🤬": ["https://files.catbox.moe/shxwj1.mp3","https://files.catbox.moe/h9ekli.mp3"],
-      "🙈": ["https://files.catbox.moe/3qc90y.mp3"],
-      "😍": ["https://files.catbox.moe/qjfk1b.mp3"],
-      "😭": ["https://files.catbox.moe/itm4g0.mp3"],
-      "😱": ["https://files.catbox.moe/mu0kka.mp3"],
-      "😻": ["https://files.catbox.moe/y8ul2j.mp3"],
-      "😿": ["https://files.catbox.moe/tqxemm.mp3"],
-      "💔": ["https://files.catbox.moe/6yanv3.mp3"],
-      "🤣": ["https://files.catbox.moe/2sweut.mp3","https://files.catbox.moe/jl3pzb.mp3"],
-      "🥹": ["https://files.catbox.moe/jf85xe.mp3"],
-      "😩": ["https://files.catbox.moe/b4m5aj.mp3"],
-      "🫣": ["https://files.catbox.moe/ttb6hi.mp3"],
-      "🐸": ["https://files.catbox.moe/utl83s.mp3","https://files.catbox.moe/sg6ugl.mp3"],
-      "💋": ["https://files.catbox.moe/37dqpx.mp3"],
-      "🫦": ["https://files.catbox.moe/61w3i0.mp3"],
-      "😴": ["https://files.catbox.moe/rm5ozj.mp3"],
-      "🙏": ["https://files.catbox.moe/7avi7u.mp3"],
-      "😼": ["https://files.catbox.moe/4oz916.mp3"],
-      "🖕": ["https://files.catbox.moe/593u3j.mp3","https://files.catbox.moe/dtua60.mp3"],
-      "🥵": ["https://files.catbox.moe/l90704.mp3"],
-      "🙂": ["https://files.catbox.moe/4oks08.mp3"],
-      "😒": ["https://files.catbox.moe/mt5il0.mp3"],
-      "😓": ["https://files.catbox.moe/zh3mdg.mp3"],
-      "🤧": ["https://files.catbox.moe/zh3mdg.mp3"],
-      "🙄": ["https://files.catbox.moe/vgzkeu.mp3"]
-
-    };
-
-    const emoji = body.trim();
-    const audioList = emojiAudioMap[emoji];
-    if (!audioList) return;
-
-    // --- RANDOM AUDIO SELECT ---
-    const audioUrl = audioList[Math.floor(Math.random() * audioList.length)];
-
-    const cacheDir = path.join(__dirname, "cache");
-    fs.ensureDirSync(cacheDir);
-
-    // 🔥 UNIQUE FILE NAME EVERY TIME TO AVOID REPEAT
-    const filePath = path.join(
-      cacheDir,
-      `${encodeURIComponent(emoji)}_${Date.now()}_${Math.floor(Math.random() * 1000)}.mp3`
-    );
-
-    try {
-      const response = await axios.get(audioUrl, { responseType: "arraybuffer" });
-      fs.writeFileSync(filePath, Buffer.from(response.data));
-
-      // 🔥 REPLY WITH FRESH STREAM
-      await message.reply({ attachment: fs.createReadStream(filePath) });
-
-      fs.unlink(filePath, (err) => {
-        if (err) console.error("Failed to delete cache file:", err);
-      });
-    } catch (error) {
-      console.error(error);
-      message.reply("ইমোজি দিয়ে লাভ নাই 😒\nযাও মুড়ি খাও জান 😘");
-    }
-  }
+module.exports.config = {
+ name: "catsay",
+ version: "1.0.1",
+ hasPermssion: 0,
+ credits: "MAHBUB SHAON",
+ description: "",
+ commandCategory: "edit-img",
+ cooldowns: 0,
+ dependencies: {
+ "fs-extra": "",
+ "request": ""
+ }
 };
+module.exports.run = async ({ api, event,args }) => { {
+ 
+ const fs = global.nodemodule["fs-extra"];
+ const request = global.nodemodule["request"];
+	 const { threadID, messageID, senderID, body } = event;
+	let text = args.toString().replace(/,/g, ' ');
+if (!text)
+ return api.sendMessage("[Text]", event.threadID, event.messageID);
+
+	 var callback = () => api.sendMessage({body:``,attachment: fs.createReadStream(__dirname + "/cache/cat.png")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/cat.png"),event.messageID);
+	 return request(encodeURI(`https://cataas.com/cat/cute/says/${text}`)).pipe(fs.createWriteStream(__dirname+'/cache/cat.png')).on('close',() => callback()); 
+}}
