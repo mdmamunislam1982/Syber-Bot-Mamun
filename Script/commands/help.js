db329ef1d196ab635099adbb2a11501c3c47989e
mdmamunislam1982 <mamunofficial3210@gmail.com>
2026-01-27 18:05:32 +0600

    Update help.js

--- Script/commands/help.js
+++ Script/commands/help.js
@@ -6,7 +6,7 @@ module.exports.config = {
     name: "help",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "MAMUN ISLAM",
     description: "Shows all commands with details",
     commandCategory: "system",
     usages: "[command name/page number]",
