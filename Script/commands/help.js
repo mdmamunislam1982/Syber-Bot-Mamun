f510439927f28561375c6bba633d1eae0ea4fb28
mdmamunislam1982 <mamunofficial3210@gmail.com>
2026-01-28 10:22:52 +0600

    Update help.js

--- Script/commands/help.js
+++ Script/commands/help.js
@@ -41,7 +41,7 @@ module.exports.languages = {
 
 /* ✅ Direct image links only */
 const helpImages = [
-  "https://i.imgur.com/qAjChmF.jpeg",
+  ![image]"(https://i.imgur.com/qAjChmF.jpeg)",
   "https://i.imgur.com/5QnK2s9.jpg"
 ];
 
