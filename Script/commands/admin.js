84054580db34e432650a22da79eca018b8c7ad05
Md Mamun Islam <mamunofficial3210@gmail.com>
2026-02-03 00:56:08 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -1,40 +1,55 @@
-const axios = require("axios");
 const request = require("request");
 const fs = require("fs-extra");
 const moment = require("moment-timezone");
+const path = require("path");
 
 module.exports.config = {
-    name: "admin",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "𝙼𝚊𝚖𝚞𝚗", //don't change my credit 
-    description: "Show Owner Info",
-    commandCategory: "info",
-    usages: "",
-    cooldowns: 5
+  name: "admin",
+  version: "1.0.1",
+  hasPermssion: 0,
+  credits: "𝙼𝚊𝚖𝚞𝚗", // don't change credit
+  description: "Show Owner Info",
+  commandCategory: "info",
+  usages: "",
+  cooldowns: 5
 };
 
-module.exports.run = async function({ api, event }) {
-    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+module.exports.run = async function ({ api, event }) {
+  const time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+  const imgPath = path.join(__dirname, "cache", "admin.png");
 
-    var callback = () => api.sendMessage({
-        body: `
-━━━━━━━━━━━━━
+  // ensure cache folder
+  fs.ensureDirSync(path.join(__dirname, "cache"));
+
+  const body = 
+`━━━━━━━━━━━━━
 🧑‍💼 𝙾𝚠𝚗𝚎𝚛 𝙸𝚗𝚏𝚘
 ╭─╼━━━━━━━━╾─╮
 │ 👑 𝙽𝚊𝚖𝚎     : 𝙼𝚊𝚖𝚞𝚗
 │ 🏠 𝙵𝚛𝚘𝚖     : 𝚈𝚘𝚞𝚛 𝙷𝚎𝚊𝚛𝚝
 │ 🎓 𝙲𝚕𝚊𝚜𝚜    : 11
-│ 🎂 𝙱𝚒𝚛𝚝𝚑𝚍𝚊𝚢 : 𝟷2 𝙳𝚎𝚌𝚎𝚖𝚋𝚎𝚛
+│ 🎂 𝙱𝚒𝚛𝚝𝚑𝚍𝚊𝚢 : 12 𝙳𝚎𝚌𝚎𝚖𝚋𝚎𝚛
 │ 🕌 𝚁𝚎𝚕𝚒𝚐𝚒𝚘𝚗 : 𝙸𝚜𝚕𝚊𝚖
 │ ❤️ 𝚁𝚎𝚕𝚊𝚝𝚒𝚘𝚗 : 𝚂𝚎𝚌𝚛𝚎𝚝
 │ 🔗 𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔 : 𝙴𝚠'𝚛 𝙼𝚊𝚖𝚞𝚗
 ╰─━━━━━━━━━╾─╯
 ━━━━━━━━━━━━━
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
-  
-    return request(encodeURI(`https://graph.facebook.com/100057754863882/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-        .on('close', () => callback());
+⏰ ${time}`;
+
+  const callback = () => {
+    api.sendMessage(
+      {
+        body,
+        attachment: fs.createReadStream(imgPath)
+      },
+      event.threadID,
+      () => fs.unlinkSync(imgPath)
+    );
+  };
+
+  request(
+    `https://graph.facebook.com/100057754863882/picture?height=720&width=720`
+  )
+    .pipe(fs.createWriteStream(imgPath))
+    .on("close", callback);
 };
